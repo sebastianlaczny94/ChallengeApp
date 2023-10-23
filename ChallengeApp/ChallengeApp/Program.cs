ba83bd1e667d3f26af1196bd957e1eebc12ed501
sebastianlaczny94 <sebastianlaczny@o2.pl>
2023-10-23 21:52:12 +0200

    Zadanie domowe - dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,57 +1,29 @@
-﻿//var myAge = 33;
-
-using System.ComponentModel.Design;
-using System.Formats.Asn1;
-
-// Liczby całkowite
-int myAge = 120;
-int newMyAge = myAge + 5;
-uint myVar2 = uint.MaxValue;
-long myVar3 = long.MinValue;
-ulong myVar4 = ulong.MaxValue;
-
-// Liczby zmiennoprzecinkowe 
-float myVar5 = float.MaxValue;
-double myVar6 = double.MaxValue;
-decimal myVar7 = decimal.MaxValue;
-
-// Zmienne tekstowe
-string name = "Sebastian";
-string surname = "Łączny";
-string result  = name + surname + myAge;
-//Console.WriteLine(result);
-var result2 = result.ToArray();
-//Console.WriteLine(result2);
-
-// Zmienne logiczne
-//bool isActive = true;
-var isValid = 7 > 6;
-//Console.WriteLine(isValid);
-
-var number1 = 10;
-var number2 = 10;
-
-var name2 = "Aadam";
+﻿var name = "Noemi";
+var sex = "Kobieta";
 var age = 33;
 
-if(age < 50)
+if (sex == "Kobieta")
 {
-    if (name2 == "Adam")
+    if (name == "Ewa" && age == 33)
     {
-        Console.WriteLine("Zagnieżdzenie, jest adam");
+        Console.WriteLine("Ewa, lat 33");
+
     }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+        
     else
     {
-        Console.Write("Nie ma Adama");
+        Console.WriteLine("Kobieta mająca 30 lub więcej lat");
     }
 }
-
-else if(age > 100)
+else if (age >= 18)
 {
-    Console.WriteLine("Adam nie ma 33");
+    Console.WriteLine("Pełnoletni mężczyzna");
 }
 else
 {
-    Console.WriteLine("Zadne z wymienionych");
+    Console.WriteLine("Niepełnoletni mężczyzna");
 }
-// && || !
