f29247175b6dc33719640124e7bc60bb744354b8
sebastianlaczny94 <sebastianlaczny@o2.pl>
2023-10-25 23:21:48 +0200

    Zadanie domowe - dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,29 +1,72 @@
-﻿var name = "Noemi";
-var sex = "Kobieta";
-var age = 33;
+﻿int number = 20051;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
 
-if (sex == "Kobieta")
-{
-    if (name == "Ewa" && age == 33)
-    {
-        Console.WriteLine("Ewa, lat 33");
+int[] numbers = new int[10];
 
-    }
-    else if (age < 30)
-    {
-        Console.WriteLine("Kobieta poniżej 30 lat");
-    }
-        
-    else
-    {
-        Console.WriteLine("Kobieta mająca 30 lub więcej lat");
-    }
-}
-else if (age >= 18)
+numbers[0] = 0;
+numbers[1] = 0;
+numbers[2] = 0;
+numbers[3] = 0;
+numbers[4] = 0;
+numbers[5] = 0;
+numbers[6] = 0;
+numbers[7] = 0;
+numbers[8] = 0;
+numbers[9] = 0;
+
+int i = 0;
+foreach (char letter in letters)
 {
-    Console.WriteLine("Pełnoletni mężczyzna");
+
+    if (letter == '0')
+    {
+        numbers[0]++;
+    }
+    else if (letter == '1')
+    {
+        numbers[1]++;
+    }
+    else if (letter == '2')
+    {
+        numbers[2]++;
+    }
+    else if (letter == '3')
+    {
+        numbers[3]++;
+    }
+    else if (letter == '4')
+    {
+        numbers[4]++;
+    }
+    else if (letter == '5')
+    {
+        numbers[5]++;
+    }
+    else if (letter == '6')
+    {
+        numbers[6]++;
+    }
+    else if (letter == '7')
+    {
+        numbers[7]++;
+    }
+    else if (letter == '8')
+    {
+        numbers[8]++;
+    }
+    else if (letter == '9')
+    {
+        numbers[9]++;
+    }
 }
-else
+foreach (var num in numbers)
 {
-    Console.WriteLine("Niepełnoletni mężczyzna");
+    if (i == 0)
+    {
+        Console.WriteLine($"Wynik dla {number}:");
+    }
+    Console.WriteLine($"{i}> {num}");
+    i++;
 }
+
